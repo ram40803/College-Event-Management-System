ede918189b904d6cf59fed2dd559da425690bf2c
Ram Patidar <rampatidar40803@gmail.com>
2025-09-22 20:30:22 +0530

    creating event service

---
+++ eventservice/src/main/java/com/eventsystem/eventservice/model/Event.java
@@ -0,0 +1,27 @@
+package com.eventsystem.eventservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private LocalDateTime date;
+    private String location;
+    private String organizer;
+}
