6d3491f46653930710bcdeee1bde79a2cae87e0b
Ram Patidar <rampatidar40803@gmail.com>
2025-09-22 20:59:49 +0530

    creating EventService class

--- eventservice/src/main/java/com/eventsystem/eventservice/service/EventService.java
+++ eventservice/src/main/java/com/eventsystem/eventservice/service/EventService.java
@@ -1,4 +1,30 @@
 package com.eventsystem.eventservice.service;
 
+import com.eventsystem.eventservice.model.Event;
+import com.eventsystem.eventservice.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class EventService {
+
+    @Autowired
+    private EventRepository eventRepo;
+
+    public Event createEvent(Event event){
+        return eventRepo.save(event);
+    }
+
+    public List<Event> getAllEvents(){
+        return eventRepo.findAll();
+    }
+
+    public Optional<Event> getEventById(Long id){
+        return eventRepo.findById(id);
+    }
+
+    public void deleteById(Long id){
+        eventRepo.deleteById(id);
+    }
 }
