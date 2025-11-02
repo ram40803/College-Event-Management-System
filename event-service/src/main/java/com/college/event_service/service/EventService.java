3d7aa9b9257452d875ff2f43083186976e049e93
Ram Patidar <rampatidar40803@gmail.com>
2025-11-02 15:55:24 +0530

    create update function in event service

--- event-service/src/main/java/com/college/event_service/service/EventService.java
+++ event-service/src/main/java/com/college/event_service/service/EventService.java
@@ -29,4 +29,27 @@ public class EventService {
     public void deleteById(Long id){
         eventRepo.deleteById(id);
     }
+
+    public void updateEvent(Long id, Event updatedEvent) {
+        eventRepo.findById(id).map(event -> {
+            if(updatedEvent.getName() != null)
+                event.setName(updatedEvent.getName());
+            if(updatedEvent.getDescription() != null)
+                event.setDescription(updatedEvent.getDescription());
+            if(updatedEvent.getStatus() != null)
+                event.setStatus(updatedEvent.getStatus());
+            if(updatedEvent.getMaxParticipantsCapacity() != 0)
+                event.setMaxParticipantsCapacity(updatedEvent.getMaxParticipantsCapacity());
+            if(updatedEvent.getCurrentParticipants() >= 0)
+                event.setCurrentParticipants(updatedEvent.getCurrentParticipants());
+            if(updatedEvent.getDate() != null)
+                event.setDate(updatedEvent.getDate());
+            if(updatedEvent.getLocation() != null)
+                event.setLocation(updatedEvent.getLocation());
+            if(updatedEvent.getOrganizer() != null)
+                event.setOrganizer(updatedEvent.getOrganizer());
+
+            return eventRepo.save(event);
+        }).orElseThrow(() -> new RuntimeException("Event not found"));
+    }
 }
